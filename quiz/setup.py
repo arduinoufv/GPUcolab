e9083bffc35789c722d8a44230f00e1cec501c02
Ricardo Ferreira <arduinoufv@gmail.com>
2023-01-16 19:07:34 -0300

    Update setup.py

--- quiz/setup.py
+++ quiz/setup.py
@@ -1,9 +1,10 @@
 from setuptools import find_packages, setup
 setup(
     name='quizlib',
-    packages=find_packages(),
+    packages=find_packages(include=['quizlib']),
     version='0.1.0',
     description='quiz library',
     author='Ricardo Ferreira',
     license='MIT',
+    install_requires=[]
 )
