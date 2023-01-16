7c0676ff5b80f9db1320caf4714de5088eb325fc
Ricardo Ferreira <arduinoufv@gmail.com>
2023-01-16 19:03:20 -0300

    Create setup.py

---
+++ quiz/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name='quizlib',
+    packages=find_packages(),
+    version='0.1.0',
+    description='quiz library',
+    author='Ricardo Ferreira',
+    license='MIT',
+)
